dbbd87f06dc5c4fb0aa4e555ba85b158f0da83da
Mytheeswaran <mytheeswaran93@gmail.com>
2023-10-03 23:48:28 +0100

    prettier and linting

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
-import React,{useState} from 'react';
-import './styles.css';
-import reactImage from  './react.png';
-import atomSvg from './atom.svg';
+import React from 'react'
+import './styles.css'
+import reactImage from './react.png'
+import atomSvg from './atom.svg'
 
-export const App = ()=>{
-    return(
-        <>
-            <h1>React App with Webpack and TS - {process.env.NODE_ENV} - {process.env.name}</h1>
-            <img src={reactImage} alt='React Logo' width='300' height='200' />
-            <img src={atomSvg} alt='React atom svg' width='300'/>
-        </>
-    )
+export const App = () => {
+  return (
+    <>
+      <h1>
+        React App with Webpack and TS - {process.env.NODE_ENV} -{' '}
+        {process.env.name}
+      </h1>
+      <img src={reactImage} alt="React Logo" width="300" height="200" />
+      <img src={atomSvg} alt="React atom svg" width="300" />
+    </>
+  )
 }
--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,15 +1,15 @@
 const webpack = require('webpack')
 
 module.exports = {
-    mode: 'development', // sets process.env.NODE_ENV to development
-    devtool: 'cheap-module-source-map',
-    devServer:{
-        hot: true,
-        open: true // similar to cmd in webpack cli: webpack serve --config webpack/webpack.config.js --env env=dev --open
-    },
-    plugins: [
-        new webpack.DefinePlugin({
-            'process.env.name': JSON.stringify('Mythee-Dev')
-        })
-    ]
+  mode: 'development', // sets process.env.NODE_ENV to development
+  devtool: 'cheap-module-source-map',
+  devServer: {
+    hot: true,
+    open: true, // similar to cmd in webpack cli: webpack serve --config webpack/webpack.config.js --env env=dev --open
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.name': JSON.stringify('Mythee-Dev'),
+    }),
+  ],
 }
