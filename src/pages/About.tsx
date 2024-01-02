7d7d28f439a5173b53470ce8153a267e9d8149a8
Mytheeswaran <mytheeswaran93@gmail.com>
2024-01-02 11:04:44 +0000

    debounce implementation

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,3 +1,38 @@
+import { Form } from 'react-bootstrap'
+import React, { useState } from 'react'
+
 export function About(): JSX.Element {
-  return <>About-Page</>
+  const [defaultInput, setDefaultInput] = useState('')
+  const [debouncedInput, setDebouncedInput] = useState('')
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const debounce = function (cb: (...args: any[]) => void) {
+    let timer: number = 0
+
+    return (...args: unknown[]) => {
+      clearInterval(timer)
+      timer = window.setTimeout(() => {
+        cb(...args)
+      }, 1000)
+    }
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const handleChange = debounce((...args: any[]) => {
+    setDebouncedInput(args[0])
+  })
+
+  return (
+    <>
+      <Form.Group className="mb-3">
+        <Form.Label>Search about</Form.Label>
+        <Form.Control
+          onChange={(e) => handleChange(e.target.value)}
+        ></Form.Control>
+      </Form.Group>
+
+      <div>Default Input: {defaultInput}</div>
+      <div>Debounced Input: {debouncedInput}</div>
+    </>
+  )
 }
