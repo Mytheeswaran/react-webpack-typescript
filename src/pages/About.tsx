f10f1b207d144d4a9bd97e098934685614c0d6af
Mytheeswaran <mytheeswaran93@gmail.com>
2024-01-09 17:57:26 +0000

    debounce cleanup

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -37,3 +37,82 @@ export function About(): JSX.Element {
     </>
   )
 }
+
+{
+  /* 
+    Older versions of debounce before the top code
+    ==========================================Non-generic debounce function==============
+
+    export function About(): JSX.Element {
+      const [defaultInput, setDefaultInput] = useState('')
+      const [debouncedInput, setDebouncedInput] = useState('')
+
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      const debounce = function () {
+        let timer: number = 0
+
+        return (...args: unknown[]) => {
+          clearInterval(timer)
+          timer = window.setTimeout(() => {
+            setDebouncedInput(args[0])
+          }, 1000)
+        }
+      }
+
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      const handleChange = debounce()
+
+      return (
+        <>
+          <div>About-Page</div>
+          <Form.Group className="mb-3">
+            <Form.Label>Search about</Form.Label>
+            <Form.Control
+              onChange={(e) => handleChange(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+
+          <div>Default Input: {defaultInput}</div>
+          <div>Debounced Input: {debouncedInput}</div>
+        </>
+      )
+    }
+
+    =============================================generic debounce function using callback=========
+
+    export function About(): JSX.Element {
+      const [defaultInput, setDefaultInput] = useState('')
+      const [debouncedInput, setDebouncedInput] = useState('')
+
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      const debounce = function (cb) {
+        let timer: number = 0
+
+        return (...args: unknown[]) => {
+          clearInterval(timer)
+          timer = window.setTimeout(() => {
+            cb(...args)
+          }, 1000)
+        }
+      }
+
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      const handleChange = debounce((...args) => {
+        setDebouncedInput(args[0])
+      })
+
+      return (
+        <>
+          <div>About-Page</div>
+          <Form.Group className="mb-3">
+            <Form.Label>Search about</Form.Label>
+            <Form.Control onChange={(e) => handleChange(e.target.value)}></Form.Control>
+          </Form.Group>
+
+          <div>Default Input: {defaultInput}</div>
+          <div>Debounced Input: {debouncedInput}</div>
+        </>
+      )
+    }
+  */
+}
