eb04cc9bf9f80a8c893652174e1731304242324e
Mytheeswaran <mytheeswaran93@gmail.com>
2024-03-19 00:55:03 +0000

    styled components Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { Home } from './pages/Home'
 
 import { GlobalStyle } from './globalStyles'
+import Navbar from './components/navbar/Navbar'
 import { Signup } from './pages/SignupPage'
 import { Pricing } from './pages/PricingPage'
 
@@ -12,8 +13,11 @@ export const App = (): JSX.Element => {
   return (
     <Router>
       <GlobalStyle />
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/pricing" element={<Pricing />} />
       </Routes>
     </Router>
   )
